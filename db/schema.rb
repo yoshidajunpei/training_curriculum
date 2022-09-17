182c9b27aa49009df388ee504a2c23b1cf17a270
yoshidajunpei <111556965+yoshidajunpei@users.noreply.github.com>
2022-09-17 14:23:49 +0900

    Revert "Merge pull request #8 from yoshidajunpei/first_trainig"
    This reverts commit 1d5e39dd6d66cf4f45dd5b01836e95e2a3db1fb8, reversing
    changes made to d9c8d48e03bb9abe5595f4abe5f961379bd0f317.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_15_123340) do
-
-  create_table "calenders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2020_04_14_072724) do
 
   create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "plan"
